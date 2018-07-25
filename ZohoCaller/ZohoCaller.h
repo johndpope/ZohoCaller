988a2f7839b6134b6cfe55bb02fd3a5dca6e315e
manikandan.bangaru@zohocorp.com <manikandan.bangaru@zohocorp.com>
2018-07-25 14:18:13 +0530

    Initial Commit

---
+++ ZohoCaller/ZohoCaller.h
@@ -0,0 +1,19 @@
+//
+//  ZohoCaller.h
+//  ZohoCaller
+//
+//  Created by manikandan bangaru on 25/07/18.
+//  Copyright © 2018 manikandan bangaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZohoCaller.
+FOUNDATION_EXPORT double ZohoCallerVersionNumber;
+
+//! Project version string for ZohoCaller.
+FOUNDATION_EXPORT const unsigned char ZohoCallerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZohoCaller/PublicHeader.h>
+
+
---
+++ ZohoCallerTests/ZohoCallerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZohoCallerTests.swift
+//  ZohoCallerTests
+//
+//  Created by manikandan bangaru on 25/07/18.
+//  Copyright © 2018 manikandan bangaru. All rights reserved.
+//
+
+import XCTest
+@testable import ZohoCaller
+
+class ZohoCallerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
